5ad69393c2abc3db2c2c2cc8fe30be0eb610d547
Safa BENABDESSADOK <benabdessadok.safa@gmail.com>
2023-12-17 22:02:36 +0100

    Create 2 microservice from scratch

---
+++ payment-service/src/main/java/com/javatechie/paymentservice/api/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.javatechie.paymentservice.api.controller;
+
+import com.javatechie.paymentservice.api.entity.Payment;
+import com.javatechie.paymentservice.api.service.PaymentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService service;
+
+    @PostMapping("/doPayment")
+    public Payment doPayment(@RequestBody Payment payment) {
+        return service.doPayment(payment);
+    }
+}
---
+++ payment-service/src/main/java/com/javatechie/paymentservice/api/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.javatechie.paymentservice.api.repository;
+
+import com.javatechie.paymentservice.api.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment,Integer> {
+}
