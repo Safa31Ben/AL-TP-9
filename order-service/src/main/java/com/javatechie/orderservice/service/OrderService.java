14542d630dfae71b58ae4921c9b2e6d0947c3ce8
Safa BENABDESSADOK <benabdessadok.safa@gmail.com>
2023-12-21 13:39:58 +0100

    Spring cloud config server using Git to Centralize configuration across application

--- order-service/src/main/java/com/javatechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/javatechie/orderservice/service/OrderService.java
@@ -6,18 +6,25 @@ import com.javatechie.orderservice.common.TransactionResponse;
 import com.javatechie.orderservice.entity.Order;
 import com.javatechie.orderservice.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
 
     @Autowired
     private OrderRepository repository;
 
     @Autowired
+    @Lazy
     private RestTemplate template;
 
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_URL;
     public TransactionResponse saveOrder(TransactionRequest request) {
         String response = "";
         Order order = request.getOrder();
@@ -25,7 +32,7 @@ public class OrderService {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         Payment paymentResponse =
-                template.postForObject("http://PAYMENT-SERVICE/payment/doPayment", payment, Payment.class);
+                template.postForObject(ENDPOINT_URL, payment, Payment.class);
 
         response = paymentResponse.getPaymentStatus().equals("success") ?
                 "payment processing successful and order placed":
